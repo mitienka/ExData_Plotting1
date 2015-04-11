ff18eaf9a074cad2ecbb726ae9d6a053d3fc3a52
Frederic Grenier <fr3d3ric.grenier@gmail.com>
2015-04-11 23:36:05 +0200

    1st version, seems ok

---
+++ plot1.R
@@ -0,0 +1,19 @@
+
+plot1 <- function() {
+
+# the file is supposed in the same folder as the script
+file <- file.path(".","household_power_consumption.txt", fsep = .Platform$file.sep)
+
+# this script uses the sqldf library to read only the required days from the data file
+library(sqldf)
+
+DATA <- read.csv.sql(file, sql = "select * from file where Date in ('1/2/2007','2/2/2007')", header=TRUE, sep = ";")
+
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+with(DATA, hist(Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power"))
+
+dev.off()
+
+}
